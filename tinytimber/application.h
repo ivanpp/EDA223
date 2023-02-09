f097266668f0e594c71eb239a930a9cfd959c68f
ivanpp <ivanding94@gmail.com>
2023-02-09 11:43:40 +0100

    Minor fix

--- tinytimber/application.h
+++ tinytimber/application.h
@@ -2,18 +2,17 @@
 #define APPLICATION_API
 
 #define MAX_BUFFER_SIZE 4
-#define MAX_HISTORY_SIZE 8
+#define MAX_HISTORY_SIZE 5
 #define NHISTORY 3
 
 #define KEY_MAX 5
 #define KEY_MIN -5
 // pre-computed periods, [-15, 16]
 #define PERIODS_IDX_DIFF -15
-const int PERIODS [32] = {2717, 2564, 2415, 2272, 2145, 2032, 1915, 1805 /*-8*/, 
-                          1706, 1607, 1519, 1432, 1355, 1278, 1204, 1136 /*0*/, 
-                          1072, 1014, 956, 902, 851, 803, 758, 716 /*8*/, 
-                          676, 638, 602, 568, 536, 506, 478, 451 /*16*/};
-
+const int PERIODS [32] = {2702, 2551, 2407, 2272, 2145, 2024, 1911, 1803, /*-8*/
+						  1702, 1607, 1516, 1431, 1351, 1275, 1203, 1136 /*0*/, 
+						  1072, 1012, 955, 901, 851, 803, 758, 715 /*8*/, 
+						  675, 637, 601, 568, 536, 506, 477, 450 /*16*/};
 
 typedef struct {
     Object super;
