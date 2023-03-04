69692ba924af7792b579128e5a49c99516ac74e3
ivanpp <ivanding94@gmail.com>
2023-03-04 02:18:22 +0100

    Some blank to fill

--- tinytimber/userButton.c
+++ tinytimber/userButton.c
@@ -7,27 +7,46 @@ void reactUserButton(UserButton *self, int unused){
     int currentStatus = SIO_READ(&sio0);
     char releasedInfo [64] = {};
     char pressedInfo [32] = {};
-    // default behavior: trigger call-back when released->pressed
-    if (currentStatus == PRESSED){
-        T_RESET(&self->timer);
-        snprintf(pressedInfo, 32, "Button Pressed\n");
-        SCI_WRITE(&sci0, pressedInfo);
-        SIO_TRIG(&sio0, 1);
+    // PRESS_MOMENTARY mode
+    if (self->mode == PRESS_MOMENTARY){
+        if (currentStatus == PRESSED){
+            T_RESET(&self->timer);
+            snprintf(pressedInfo, 32, "Button Pressed\n");
+            SCI_WRITE(&sci0, pressedInfo);
+            SIO_TRIG(&sio0, 1);
+        } else {           //RELEASED
+            int duration_sec, duration_msec;
+            duration_sec = SEC_OF(T_SAMPLE(&self->timer));
+            duration_msec = MSEC_OF(T_SAMPLE(&self->timer));
+            snprintf(releasedInfo, 64,
+                "Button Released: duration: %d s %d ms\n", duration_sec, duration_msec);
+            SCI_WRITE(&sci0, releasedInfo);
+            SIO_TRIG(&sio0, 0);
+            // PRESS_MOMENTARY -> PRESS_AND_HOLD
+            if (duration_sec >= 1){
+                self->mode = PRESS_AND_HOLD;
+                SCI_WRITE(&sci0, "the fact (I'm funny)\n");
+                SCI_WRITE(&sci0, "PRESS_AND_HOLD MODE\n");
+            }
+        }
     }
-    if (currentStatus == RELEASED){
-        int duration_sec, duration_msec;
-        duration_sec = SEC_OF(T_SAMPLE(&self->timer));
-        duration_msec = MSEC_OF(T_SAMPLE(&self->timer));
-        snprintf(releasedInfo, 64,
-            "Button Released: duration: %d s %d ms\n", duration_sec, duration_msec);
-        SCI_WRITE(&sci0, releasedInfo);
-        SIO_TRIG(&sio0, 0);
-        if (duration_sec >= 1){
-            // change mode
+    // PRESS_AND_HOLD mode, you should speak less in this mode
+    if (self->mode == PRESS_AND_HOLD){
+        
+        if (currentStatus == PRESSED){
+            T_RESET(&self->timer);
+            SIO_TRIG(&sio0, 1);
+        } else {           //RELEASED
+            int duration_sec, duration_msec;
+            duration_sec = SEC_OF(T_SAMPLE(&self->timer));
+            duration_msec = MSEC_OF(T_SAMPLE(&self->timer));
+            snprintf(releasedInfo, 64, 
+                "Stuck in PRESS_AND_HOLD MODE\n");
+            SCI_WRITE(&sci0, releasedInfo);
+            SIO_TRIG(&sio0, 0);
         }
     }
 }
-
 
 // DEPRECATED
 // 'background load' for button
