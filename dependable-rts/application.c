58cfce39ea1fa36a7c9dd1dc8d77fa25134779f7
Oliver Karmetun <oliver.k@live.se>
2023-04-18 01:59:59 +0200

    Fix: Switch to reactP1

--- dependable-rts/application.c
+++ dependable-rts/application.c
@@ -14,7 +14,7 @@ UserButton userButton = initUserButton();
 
 Serial sci0 = initSerial(SCI_PORT0, &app, reader);
 Can can0 = initCan(CAN_PORT0, &app, receiver);
-SysIO sio0 = initSysIO(SIO_PORT0, &userButton, reactUserButton);
+SysIO sio0 = initSysIO(SIO_PORT0, &userButton, reactUserButtonP1);
 
 
 /* CAN MSG */
